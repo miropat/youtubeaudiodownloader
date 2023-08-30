896832173dfd046bb6cbcc88cc19a2757a4c56ef
miropat <93606923+miropat@users.noreply.github.com>
2023-08-30 21:04:26 +0300

    audio downloader

---
+++ youtubeAudioDownloader.py
@@ -0,0 +1,25 @@
+from pytube import YouTube
+from moviepy.editor import *
+
+
+
+video_url = input("Please enter the video URL: ")
+yt = YouTube(video_url)
+
+
+
+#print("View: ", yt.views)
+
+video = yt.streams.filter(only_audio=True, file_extension="mp4").first()
+
+
+video.download()
+#convert audio
+
+original_filename = video.default_filename
+new_filename = original_filename.replace(".mp4", ".mp3")
+import os
+os.rename(original_filename, new_filename)
+
+print("Title: ", yt.title + " has been downloaded and converted")
+
